4efb14a5b7277fc8d58a3d25f7da563de35bf188
isaias <isaias0005@gmail.com>
2023-04-15 14:05:50 -0400

    added preact

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use std::sync::{Arc, Mutex};
 
-use axum::{extract::State, routing::get, Json, Router, Server, response::{IntoResponse, Html}};
+use axum::{extract::State, routing::get, Json, Router, Server, response::{IntoResponse, Html}, http::Response};
 use sysinfo::{CpuExt, System, SystemExt};
 
 #[tokio::main]
 async fn main() {
     let router = Router::new()
         .route("/", get(root_get))
+        .route("/index.mjs", get(indexmjs_get))
         .route("/api/cpus", get(cpus_get))
         .with_state(AppState {
             sys: Arc::new(Mutex::new(System::new())),
@@ -26,6 +27,16 @@ struct AppState {
 async fn root_get() -> impl IntoResponse {
     let markup = tokio::fs::read_to_string("src/index.html").await.unwrap();
     Html(markup)
+}
+
+#[axum::debug_handler]
+async fn indexmjs_get() -> impl IntoResponse {
+    let markup = tokio::fs::read_to_string("src/index.js").await.unwrap();
+
+    Response::builder()
+        .header("content-type", "application/javascript")
+        .body(markup)
+        .unwrap()
 }
 
 #[axum::debug_handler]
